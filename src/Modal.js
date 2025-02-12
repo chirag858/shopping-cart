ac0b119ed2b819616df1682ad9d135085eccba45
Chirag Arora <arorachirag858@gmail.com>
2025-02-12 13:03:17 +0530

    add product modal in progress

--- src/Modal.js
+++ src/Modal.js
@@ -1,19 +1,40 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import Modal from "react-modal";
 
 const AddProductModal = ({ isOpen, onRequestClose }) => {
-    let [modalIsOpen,setModalIsOpen] = useState(false);
-    console.log(isOpen);
-    // setModalIsOpen(isOpen);
-    console.log("hjdhsdh");
-
+    let [modalIsOpen, setModalIsOpen] = useState(false);
+    let [inputValue, setInputValue] = useState("");
+    let [inputNumber, setInputNumber] = useState(0);
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+        }
+    };
     if (!isOpen) return null;
     return (
-        <Modal isOpen={isOpen} onRequestClose={() => setModalIsOpen(false)} contentLabel="Example Modal">
-                <h2>Modal Title</h2>
-                <p>Modal content goes here...</p>
-                <button onClick={() => onRequestClose()}>Close</button>
-              </Modal>
+        <div>
+            <Modal isOpen={isOpen} onRequestClose={() => setModalIsOpen(false)} contentLabel="Example Modal">
+                <button onClick={() => onRequestClose()} style={{ display: "flex", float: "right" }}>Close</button>
+                <h2>Add Products here</h2>
+                <input
+                    className="input-text"
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={handleKeyPress}
+                    placeholder="Enter a new Product..."
+                />
+                <input
+                    className="input-text"
+                    type="number"
+                    value={inputNumber}
+                    onChange={(e) => setInputNumber(e.target.value)}
+                    onKeyDown={handleKeyPress}
+                    placeholder="Enter a Price"
+                />
+                <button disabled={!inputNumber || !inputValue} onClick={handleKeyPress}>Add Product</button>
+                <br></br>
+            </Modal>
+        </div>
     );
 };
 
