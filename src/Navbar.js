a178e6d62cdb289530cab45536b411c3daf2a18b
Chirag Arora <arorachirag858@gmail.com>
2025-02-26 08:31:46 +0530

    fixes

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
             <Link to="/" className="text-white text-lg font-medium hover:text-gray-300 transition duration-300">Home</Link>
           </li>
           <li>
-            <Link to="/about" className="text-white text-lg font-medium hover:text-gray-300 transition duration-300">About Us</Link>
+            <Link to="/aboutus" className="text-white text-lg font-medium hover:text-gray-300 transition duration-300">About Us</Link>
           </li>
           <li>
             <Link to="/products" className="text-white text-lg font-medium hover:text-gray-300 transition duration-300">Products</Link>
