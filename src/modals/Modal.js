957b8e5e3a4ff232672ed4c4c4e367dc217c5de7
Chirag Arora <arorachirag858@gmail.com>
2025-03-05 18:07:50 +0530

    category values

--- src/modals/Modal.js
+++ src/modals/Modal.js
@@ -6,11 +6,13 @@ import { useCart } from "../context/CartContextProvider";
 const AddProductModal = ({ isOpen, onRequestClose, onSave }) => {
     const { products, addToCart } = useCart();
     const [inputValue, setInputValue] = useState("");
+    const [inputCategoryValue, setInputCategoryValue] = useState("");
     const [inputNumber, setInputNumber] = useState("");
 
     const handleSave = () => {
         onSave({
             name: inputValue,
+            category: inputCategoryValue,
             price: parseFloat(inputNumber) || 0,
         });
         setInputValue("");
@@ -32,16 +34,27 @@ const AddProductModal = ({ isOpen, onRequestClose, onSave }) => {
                 Close
             </button>
             <h2>Add Products</h2>
+            <div style={{display : "flex", justifyContent :"space-between", width : "800px"}}>
             <input
                 className="input-text"
                 type="text"
+                style={{width : "320px",height : "20px"}}
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
                 placeholder="Enter a product name..."
             />
             <input
                 className="input-text"
+                type="text"
+                style={{width : "320px",height : "20px"}}
+                value={inputCategoryValue}
+                onChange={(e) => setInputCategoryValue(e.target.value)}
+                placeholder="Catgory"
+            />
+            <input
+                className="input-text"
                 type="number"
+                style={{width : "320px",height : "20px"}}
                 value={inputNumber}
                 onChange={(e) => setInputNumber(e.target.value)}
                 placeholder="Enter a price"
@@ -52,6 +65,7 @@ const AddProductModal = ({ isOpen, onRequestClose, onSave }) => {
             >
                 Add Product
             </button>
+            </div>
             <br />
             <ProductList products={products} addToCart={addToCart} onlyList />
         </Modal>
